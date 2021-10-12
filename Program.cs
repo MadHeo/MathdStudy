100284a9a838fb4d809ef4a28346e575eaed5796
kwangkki_Heo <tnlfl288@gmail.com>
2021-10-12 22:05:57 +0900

    study : overriding

--- Program.cs
+++ Program.cs
@@ -483,54 +483,101 @@ namespace MethodPractice
     //}
 
     // 기반 클래스와 파생 클래스 사이의 형식 변환, 그리고 is as
-    class Mammal
-    {
-        public void Nurse()
-        {
-            Console.WriteLine("Nurse()");
-        }
-    }
+    //class Mammal
+    //{
+    //    public void Nurse()
+    //    {
+    //        Console.WriteLine("Nurse()");
+    //    }
+    //}
 
-    class Dog : Mammal
-    {
-        public void Bark()
-        {
-            Console.WriteLine("Bark()");
-        }
-    }
+    //class Dog : Mammal
+    //{
+    //    public void Bark()
+    //    {
+    //        Console.WriteLine("Bark()");
+    //    }
+    //}
 
-    class Cat : Mammal
-    {
-        public void Meow()
-        {
-            Console.WriteLine("Meow()");
-        }
-    }
+    //class Cat : Mammal
+    //{
+    //    public void Meow()
+    //    {
+    //        Console.WriteLine("Meow()");
+    //    }
+    //}
 
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            Mammal mammal = new Dog();
-            Dog dog;
+    //class MainApp
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Mammal mammal = new Dog();
+    //        Dog dog;
 
-            if(mammal is Dog)
-            {
-                dog = (Dog)mammal;
-                dog.Bark();
-            }
+    //        if(mammal is Dog)
+    //        {
+    //            dog = (Dog)mammal;
+    //            dog.Bark();
+    //        }
 
-            Mammal mammal2 = new Cat();
+    //        Mammal mammal2 = new Cat();
 
-            Cat cat = mammal2 as Cat;
-            if (cat != null)
-                cat.Meow();
+    //        Cat cat = mammal2 as Cat;
+    //        if (cat != null)
+    //            cat.Meow();
 
-            Cat cat2 = mammal as Cat;
-            if (cat2 != null)
-                cat2.Meow();
-            else
-                Console.WriteLine("cat2 is not a Cat");
-        }
-    }
+    //        Cat cat2 = mammal as Cat;
+    //        if (cat2 != null)
+    //            cat2.Meow();
+    //        else
+    //            Console.WriteLine("cat2 is not a Cat");
+    //    }
+    //}
+
+    //파생클래스 오버라이드
+    //class ArmorSuite
+    //{
+    //    public virtual void Initialize()
+    //    {
+    //        Console.WriteLine("Armored");
+    //    }
+    //}
+
+    //class IronMan : ArmorSuite
+    //{
+    //    public override void Initialize()
+    //    {
+    //        base.Initialize();
+    //        Console.WriteLine("Repulsor Rays Armed");
+    //    }
+    //}
+
+    //class WarMachine : ArmorSuite
+    //{
+    //    public override void Initialize()
+    //    {
+    //        base.Initialize();
+    //        Console.WriteLine("Double-Barrel Cannons Armed");
+    //        Console.WriteLine("Micro-Rocket Launcher Arme");
+    //    }
+    //}
+
+    //class MainApp
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Creating ArmorSuite...");
+
+    //        ArmorSuite armorsuite = new ArmorSuite();
+    //        armorsuite.Initialize();
+
+    //        Console.WriteLine("\nCreating IronMan...");
+    //        ArmorSuite ironman = new IronMan();
+    //        ironman.Initialize();
+
+    //        Console.WriteLine("\nCreating WarMachine...");
+    //        ArmorSuite warmachine = new WarMachine();
+    //        warmachine.Initialize();
+    //    }
+    //}
 }
