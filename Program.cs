5a9e8d786a9d9c8aac13b468ba0ec3de9c91dab2
kwangkki_Heo <tnlfl288@gmail.com>
2021-10-17 17:25:47 +0900

    study : 211017 Split Class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MethodPractice
 {
@@ -614,41 +615,114 @@ namespace MethodPractice
     //}
 
     //오버라이딩 봉인하기
-    class Base
-    {
+    //class Base
+    //{
 
-        public virtual void SealMe()
-        {
+    //    public virtual void SealMe()
+    //    {
 
-        }
-    }
+    //    }
+    //}
 
-    class Derived : Base
-    {
-        public sealed override void SealMe()
-        {
+    //class Derived : Base
+    //{
+    //    public sealed override void SealMe()
+    //    {
 
-        }
-    }
+    //    }
+    //}
 
-    class WantToOverride : Derived
-    {
-        public override void SealMe()
-        {
+    //class WantToOverride : Derived
+    //{
+    //    public override void SealMe()
+    //    {
 
-        }
-    }
+    //    }
+    //}
 
-    class MainApp
-    {
+    //class MainApp
+    //{
 
-        static void Main(string[] args)
-        {
+    //    static void Main(string[] args)
+    //    {
 
-        }
-    }
+    //    }
+    //}
+
+
+    //중첩 클래스
+    //class Configuration
+    //{
+    //    List<ItemValue> listConfig = new List<ItemValue>();
+
+    //    public void SetConfig(string item, string value)
+    //    {
+    //        ItemValue iv = new ItemValue();
+    //        iv.SetValue(this, item, value);
+    //    }
+
+    //    public string GetConfig(string item)
+    //    {
+    //        foreach (ItemValue iv in listConfig)
+    //        {
+    //            if (iv.GetItem() == item)
+    //                return iv.GetValue();
+    //        }
+
+    //        return "";
+    //    }
+
+    //    private class ItemValue
+    //    {
+    //        private string item;
+    //        private string value;
+
+    //        public void SetValue(Configuration config, string item, string value)
+    //        {
+    //            this.item = item;
+    //            this.value = value;
+
+    //            bool found = false;
+    //            for (int i = 0; i < config.listConfig.Count; i++)
+    //            {
+    //                if (config.listConfig[i].item == item)
+    //                {
+    //                    config.listConfig[i] = this;
+    //                    found = true;
+    //                    break; 
+    //                }
+    //            }
+
+    //            if (found == false)
+    //                config.listConfig.Add(this);
+    //        }
+    //        public string GetItem()
+    //        { return item; }
+    //        public string GetValue()
+    //        { return value; }
+    //    }
+    //}
+
+
+
+    //class MainApp
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Configuration config = new Configuration();
+    //        config.SetConfig("Version", "V 5.0");
+    //        config.SetConfig("Size", "655,324 KB");
+
+    //        Console.WriteLine(config.GetConfig("Version"));
+    //        Console.WriteLine(config.GetConfig("Size"));
+
+    //        config.SetConfig("Version", "V 5.0.1");
+    //        Console.WriteLine(config.GetConfig("Version"));
+
+    //    }
+    //}
+
+    //14. 분할 클래스
     
-
-   //오
 
 }
