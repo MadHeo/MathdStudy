8b1884225499be27aa7853344bf2274bbda5c942
kwangkki_Heo <tnlfl288@gmail.com>
2021-11-10 21:20:02 +0900

    study : 211110 Arrange

--- Program.cs
+++ Program.cs
@@ -970,30 +970,30 @@ namespace MethodPractice
 
 
     //21. 배열
-    class MainApp
-    {
-        static void Main(string[] args)
-        {
-            int[] scores = new int[5];
-            scores[0] = 80;
-            scores[1] = 74;
-            scores[2] = 81;
-            scores[3] = 90;
-            scores[4] = 34;
+    //class MainApp
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int[] scores = new int[5];
+    //        scores[0] = 80;
+    //        scores[1] = 74;
+    //        scores[2] = 81;
+    //        scores[3] = 90;
+    //        scores[4] = 34;
 
-            foreach (int score in scores)
-                Console.WriteLine(score);
+    //        foreach (int score in scores)
+    //            Console.WriteLine(score);
 
-            int sum = 0;
-            foreach (int scoreo in scores)
-                sum += score;
+    //        int sum = 0;
+    //        foreach (int score in scores)
+    //            sum += score;
 
-            int average = sum / scores.Length;
+    //        int average = sum / scores.Length;
 
-            Console.WriteLine($"Average Score : {average}");
+    //        Console.WriteLine($"Average Score : {average}");
 
-        }
-    }
+    //    }
+    //}
 
 
 }
